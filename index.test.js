760d43552b60ec8f31e370bbca9bc14ff93afd1d
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-05-21 19:34:55 -0300

    Refactor test cases in index.test.js to use describe block for better organization

--- index.test.js
+++ index.test.js
@@ -1,19 +1,21 @@
 const index = require("./index");
 
-test("Apply discount", () => {
-  const result = index.applyDiscount(10, 5);
+describe("Testes de Desconto", () => {
+  test("Apply discount", () => {
+    const result = index.applyDiscount(10, 5);
 
-  expect(result).toEqual(5);
-});
+    expect(result).toEqual(5);
+  });
 
-test("Apply discount - Discount greater than value", () => {
-  const result = index.applyDiscount(10, 15);
+  test("Apply discount - Discount greater than value", () => {
+    const result = index.applyDiscount(10, 15);
 
-  expect(result).toEqual(0);
-});
+    expect(result).toEqual(0);
+  });
 
-test("Apply discount - Discount equal to value", () => {
-  const result = index.applyDiscount(10, 10);
+  test("Apply discount - Discount equal to value", () => {
+    const result = index.applyDiscount(10, 10);
 
-  expect(result).toEqual(0);
+    expect(result).toEqual(0);
+  });
 });
